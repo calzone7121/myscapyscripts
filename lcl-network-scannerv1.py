091abdb9602683b0a9822568f2222f247546e90c
cbm7121 <95052086+cbm7121@users.noreply.github.com>
2022-04-07 15:00:16 -0500

    Add files via upload

---
+++ lcl-network-scannerv1.py
@@ -0,0 +1,59 @@
+import scapy.all as scapy
+import re
+import socket
+
+
+#initialize target IP address range
+target_ip = scapy.ARP()
+#initialize destination target
+broadcast = scapy.Ether()
+#create ip range validation
+valid_ip_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+#initialize array of clients
+clients = []
+
+#main function
+def main():
+    while True:
+        get_current_ip()
+        input_range = input("Enter IP Address Range (EX: 0.0.0.0/24): ")
+        if valid_ip_pattern.search(input_range):
+            target_ip.pdst = input_range
+            print("Valid IP Address. Processing...")
+            break
+        print("Invalid IP range please try again.")
+    send_packets()
+    display_clients()
+
+#Obtain current IP address for reference of range
+def get_current_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        current_ip = s.getsockname()[0]
+    except:
+        current_ip = '127.0.0.1'
+    finally:
+        s.close()
+    print("Your Current IP address is: " + str(current_ip))
+
+#Send packets to get info and populate clients array
+def send_packets():
+    broadcast.dst='ff:ff:ff:ff:ff:ff'
+    packet = broadcast / target_ip
+    client = scapy.srp(packet, timeout = 1)[0]
+    for sent, received in client:
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+#display clients information from clients array
+def display_clients():
+    print()
+    print("No |\tIP Address     |\tMAC Address        |")
+    print("___|\t_______________|\t___________________|")
+    print()
+    for client in clients:
+        print("{}\t{}\t\t{}".format(clients.index(client), client['ip'], client['mac']))
+        print("-"*52)
+
+if __name__=="__main__":
+    main()
---
+++ lcl-nework-scannerv2-2.py
@@ -0,0 +1,59 @@
+from scapy.all import Ether, ARP, srp
+import re
+import socket
+#initialize target IP variable and valid range checker
+target_ip = ARP()
+valid_ip_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+#initialize broadcast variable (router destination)
+broadcast = Ether(dst='ff:ff:ff:ff:ff:ff')
+#initialize array of clients
+clients = []
+
+
+
+#main laucher function
+def main():
+    while True:
+        get_current_ip()
+        input_range = input("Enter IP Range (EX: 10.71.71.1/24): ")
+        if valid_ip_pattern.search(input_range):
+            print("Valid Range Entered. Processing...")
+            target_ip.pdst = input_range
+            break
+        print("Invalid Range please try again.")
+    send_packets()
+    display_clients()
+    
+
+def get_current_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        current_ip = s.getsockname()[0]
+    except:
+        current_ip = '127.0.0.1'
+    finally:
+        s.close()
+    print("Your Current IP address is: " + str(current_ip))
+
+def send_packets():
+    packet = broadcast / target_ip
+    client = srp(packet, timeout = 1)[0]
+    for sent, received in client:
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+def display_clients():
+    print()
+    print("No. |\tDevice Name     |\tIP Adddress     |\tMAC Address        |")
+    print("____|\t________________|\t________________|\t___________________|")
+    print()
+    for client in clients:
+        try:  
+            print("{}\t{:20}\t{:16}\t{:16}".format(clients.index(client), socket.gethostbyaddr(client['ip'])[0], client['ip'], client['mac']))
+            print("-"*76)
+        except:
+            print("{}\t{:20}\t{:16}\t{:16}".format(clients.index(client), "Unknown-Host", client['ip'], client['mac']))
+            print("-"*76)
+
+if __name__=="__main__":
+    main()
